50a36bdffd7ab3b1824d6cff359399eab6ee3010
Divyansh Saxena <divyanshsaxenaofficial@gmail.com>
2022-12-13 04:16:06 +0000

    modified:   streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -35,7 +35,7 @@ streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
 
-import streamlit as sl
+import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
@@ -43,23 +43,23 @@ import plotly.express as px
 from datetime import datetime
 
 # Title of App
-sl.write('Snowflake Warehouse Report')
+st.write('Snowflake Warehouse Report')
 
-sl.header('st.button')
-if sl.button('hello world'):
-    sl.write('why hello there')
+st.header('st.button')
+if st.button('hello world'):
+    st.write('why hello there')
 else:
-    sl.write('good bye')
+    st.write('good bye')
 
 # Side bar sections
-sb_loyalty = sl.sidebar.selectbox('KILO-Loyalty Reports',('Warehouse Usage','Alerts'))
-sb_teradata = sl.sidebar.selectbox('Teradata MIG Reports',('Warehouse Usage','Alerts'))
+sb_loyalty = st.sidebar.selectbox('KILO-Loyalty Reports',('Warehouse Usage','Alerts'))
+sb_teradata = st.sidebar.selectbox('Teradata MIG Reports',('Warehouse Usage','Alerts'))
 
 # KILO Loyalty
 if sb_loyalty == 'Warehouse Usage':
-    sl.write('warehouse usage')
+    st.write('warehouse usage')
     df = 1
-    sl.metric('views',df,45)
+    st.metric('views',df,45)
 
 # Side bar sections
 sb_teradata = st.sidebar.selectbox('Teradata MIG Reports',('Warehouse Usage','Alerts'))
